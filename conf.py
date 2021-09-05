caa8c67217f8da7d9c8628739177f0b3f081a12c
meowdan <edwiin.me@gmail.com>
2021-09-06 00:37:36 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ enable_jsdelivr = {
 
 # 站点设置
 site_name = "超时空蛋蛋"
-site_logo = "meowdan.png"
+site_logo = "favicon.png"
 site_build_date = "2021-09-03T16:51+08:00"
 author = "meowdan"
 email = "edwiinme@gmail.com"
